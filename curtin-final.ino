e260530c10c5f85ba69b1e497e9e8fcc01fb3bc8
Nick <x>
2016-11-29 21:36:34 -0800

    auto format

--- curtin-final.ino
+++ curtin-final.ino
@@ -10,9 +10,9 @@ const int closepin = 5;
 const int openpin = 6;
 const int motorlockpin = 11;
 int sensorPin = A0;
-int x,y,z;
-int xp,yp,zp;
-int dx,dy,dz;
+int x, y, z;
+int xp, yp, zp;
+int dx, dy, dz;
 int knockfound; // this will have a 0 if nothing, and a 1 if we found a knock
 int threshold = 20; // how sensative the knock sensor is.
 int sensorValue = 0;
@@ -39,7 +39,7 @@ void opencurtins(int count)
 {
   digitalWrite(curtinopenpin, HIGH);
   int i = 0;
-  while( i < count )
+  while ( i < count )
   {
     digitalWrite(motorlockpin, HIGH);
     digitalWrite(curtinclosepin, HIGH);
@@ -52,7 +52,7 @@ void opencurtins(int count)
 }
 
 void closecurtins(int count)
-{ 
+{
   digitalWrite(curtinclosepin, HIGH);
   int i = 0;
   while ( i < count )
@@ -76,7 +76,7 @@ void detectKnock()
 {
   // pull fresh sensor values
   x = analogRead(xpin);
-  y = analogRead(ypin); 
+  y = analogRead(ypin);
   z = analogRead(zpin);
 
   // calculate difference from previous sensor values
@@ -85,7 +85,7 @@ void detectKnock()
   dz = z - zp;
   // Note: at this point all calculations are correct
 
-  if (sensorValue != lastsensorValue || abs(dx) > threshold || abs(dy) > threshold || abs(dz) > threshold ) 
+  if (sensorValue != lastsensorValue || abs(dx) > threshold || abs(dy) > threshold || abs(dz) > threshold )
   {
     Serial.print("detect  ");
     knockfound = 1;
@@ -94,13 +94,13 @@ void detectKnock()
   {
     knockfound = 0;
   }
-     Serial.print(dx);
-     Serial.print("\t"); // a tab between values
-     Serial.print(dy);
-     Serial.print("\t"); // a tab between values
-     Serial.print(dz);
-     Serial.println();
-//  Serial.print(sensorPin);
+  Serial.print(dx);
+  Serial.print("\t"); // a tab between values
+  Serial.print(dy);
+  Serial.print("\t"); // a tab between values
+  Serial.print(dz);
+  Serial.println();
+  //  Serial.print(sensorPin);
   // move what was fresh into the previous, (discarding what was previous)
   // Note: dont do math after these 3 lines
   xp = x;
@@ -109,73 +109,73 @@ void detectKnock()
 }
 //if (sensorValue != lastsensorValue)
 //{
- //Serial.print("detect  ");
+//Serial.print("detect  ");
 //knockfound = 1;
 //}
 //else
 //{
- //knockfound = 0;
+//knockfound = 0;
 //}
 
 void loop()
 {
-  sensorValue = analogRead(sensorPin); 
+  sensorValue = analogRead(sensorPin);
 
-  float voltage= sensorValue * (3.3 / 1023.0);
+  float voltage = sensorValue * (3.3 / 1023.0);
 
-  
 
-    // jump and run this code.  this function will set the variable knockfound;
-    // "this is looking for the value"
-    detectKnock();
 
-    // "knockfound actually makes it do something"
-    if (digitalRead (closepin) == LOW && knockfound)
+  // jump and run this code.  this function will set the variable knockfound;
+  // "this is looking for the value"
+  detectKnock();
+
+  // "knockfound actually makes it do something"
+  if (digitalRead (closepin) == LOW && knockfound)
+  {
+    while (digitalRead(openpin) == HIGH)
     {
-      while(digitalRead(openpin) == HIGH)
-      {
-        opencurtins(15); // was 5680
-      }
-
-      // at this poing we always know there will be another detection
-      // burn value
-      detectKnock();
-      detectKnock();
-      detectKnock();
-      detectKnock();
+      opencurtins(15); // was 5680
     }
-    // delay(200);
-    if (digitalRead (openpin) == LOW && knockfound)
-    {
-      while(digitalRead(closepin) == HIGH)
-      {
-        closecurtins(15); // was 5680
-      }
 
-
-    }
-    //while (closepin == HIGH);
     // at this poing we always know there will be another detection
     // burn value
     detectKnock();
     detectKnock();
     detectKnock();
     detectKnock();
-
-    if (digitalRead(openpin) == HIGH && digitalRead(closepin) == HIGH && knockfound)
+  }
+  // delay(200);
+  if (digitalRead (openpin) == LOW && knockfound)
+  {
+    while (digitalRead(closepin) == HIGH)
     {
-      while(digitalRead(closepin) == HIGH)
-      {
-        closecurtins(15); // was 5680
-      }
-      detectKnock();
-      detectKnock();
-
-      //closecurtins(1000); // was 5690
-      // delay before next reading:
-      //delay(100);
+      closecurtins(15); // was 5680
     }
-  
+
+
+  }
+  //while (closepin == HIGH);
+  // at this poing we always know there will be another detection
+  // burn value
+  detectKnock();
+  detectKnock();
+  detectKnock();
+  detectKnock();
+
+  if (digitalRead(openpin) == HIGH && digitalRead(closepin) == HIGH && knockfound)
+  {
+    while (digitalRead(closepin) == HIGH)
+    {
+      closecurtins(15); // was 5680
+    }
+    detectKnock();
+    detectKnock();
+
+    //closecurtins(1000); // was 5690
+    // delay before next reading:
+    //delay(100);
+  }
+
 }
 
 
